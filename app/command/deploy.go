2faf7e47ac613cd21a7f2fd3368b6981dfc86ba2
fuxiaohei <fuxiaohei@vip.qq.com>
2015-12-09 20:37:01 +0800

    [command] fix deploy fatal affects all commands

--- app/command/deploy.go
+++ app/command/deploy.go
@@ -26,25 +26,24 @@ func Deploy(opt *builder.BuildOption) cli.Command {
 }
 
 func deploySite(opt *builder.BuildOption) func(ctx *cli.Context) {
-
-	if iniFile == nil {
-		log15.Crit("Deploy.Fail", "error", errors.New("please add conf.ini to set deploy options"))
-	}
-	deployer, err := deploy.New(iniFile)
-	if err != nil {
-		log15.Crit("Deploy.Fail", "error", err.Error())
-	}
-
-	// real deploy action, in builder hook
-	afterFunc := func(b *builder.Builder, ctx *builder.Context) error {
-		if b.IsWatching() {
-			return deployer.RunAsync(b, ctx)
-		}
-		return deployer.Run(b, ctx)
-	}
-
 	// build action
 	return func(ctx *cli.Context) {
+
+		if iniFile == nil {
+			log15.Crit("Deploy.Fail", "error", errors.New("please add conf.ini to set deploy options"))
+		}
+		deployer, err := deploy.New(iniFile)
+		if err != nil {
+			log15.Crit("Deploy.Fail", "error", err.Error())
+		}
+
+		// real deploy action, in builder hook
+		afterFunc := func(b *builder.Builder, ctx *builder.Context) error {
+			if b.IsWatching() {
+				return deployer.RunAsync(b, ctx)
+			}
+			return deployer.Run(b, ctx)
+		}
 
 		// add hook to opt
 		if opt.After == nil {
