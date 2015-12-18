9b822d0827376b437482179a4f3acd623d9c03f7
fuxiaohei <fuxiaohei@vip.qq.com>
2015-12-18 14:52:43 +0800

    [deploy] update gitTask to --dest args

--- app/builder/build_copy.go
+++ app/builder/build_copy.go
@@ -4,10 +4,11 @@ import (
 	"os"
 	"path"
 	"path/filepath"
+	"time"
 
 	"github.com/Unknwon/com"
 	"github.com/go-xiaohei/pugo-static/app/helper"
-	"time"
+    "strings"
 )
 
 // copy assets to target directory,
@@ -43,6 +44,11 @@ func (b *Builder) copyClean(ctx *Context) {
 		if fi.IsDir() {
 			return nil
 		}
+        rel, _ := filepath.Rel(ctx.DstDir, p)
+        // ignore git directory
+        if strings.HasPrefix(rel,".git"){
+            return nil
+        }
 		// not build file, clean it
 		if !ctx.Diff.Exist(p) {
 			ctx.Diff.Add(p, DIFF_REMOVE, time.Now())
--- app/deploy/git.go
+++ app/deploy/git.go
@@ -2,12 +2,13 @@ package deploy
 
 import (
 	"errors"
+	"path"
 	"strings"
 	"time"
 
 	"github.com/Unknwon/com"
 	"github.com/go-xiaohei/pugo-static/app/builder"
-	"path"
+	"gopkg.in/inconshreveable/log15.v2"
 )
 
 const (
@@ -18,6 +19,7 @@ var (
 	// _ DeployTask = new(GitTask)
 
 	ErrGitNotRepo      = errors.New("destination directory is not a git repository")
+	ErrGitNoBranch     = errors.New("can not read git respository's branch")
 	gitMessageReplacer = strings.NewReplacer("{now}", time.Now().Format(time.RFC3339))
 )
 
@@ -67,12 +69,63 @@ func (g *GitTask) Is(conf string) bool {
 	return strings.HasPrefix(conf, "git://")
 }
 
+// readRepo branch
+func (g *GitTask) readRepo(dest string) error {
+	content, _, err := com.ExecCmdDir(dest, "git", []string{"branch"}...)
+	if err != nil {
+		return err
+	}
+	contentData := strings.Split(content, "\n")
+	for _, cnt := range contentData {
+		if strings.HasPrefix(cnt, "*") {
+			cntData := strings.Split(cnt, " ")
+			g.opt.Branch = cntData[len(cntData)-1]
+			return nil
+		}
+	}
+	return nil
+}
+
 // Git deployment action
 func (g *GitTask) Do(b *builder.Builder, ctx *builder.Context) error {
 	gitDir := path.Join(ctx.DstDir, ".git")
 	if !com.IsDir(gitDir) {
 		return ErrGitNotRepo
 	}
+	var err error
+	if err = g.readRepo(ctx.DstDir); err != nil {
+		return err
+	}
+	if g.opt.Branch == "" {
+		return ErrGitNoBranch
+	}
+
+	// add files
+	if _, stderr, err := com.ExecCmdDir(ctx.DstDir, "git", []string{"add", "--all"}...); err != nil {
+		log15.Error("Deploy.Git.Error", "error", stderr)
+		return err
+	}
+	log15.Debug("Deploy.Git.[" + g.opt.Branch + "].AddFiles")
+
+	// commit message
+	message := gitMessageReplacer.Replace(g.opt.Message)
+	if _, stderr, err := com.ExecCmdDir(ctx.DstDir, "git", []string{"commit", "-m", message}...); err != nil {
+		log15.Error("Deploy.Git.Error", "error", stderr)
+		return err
+	}
+	log15.Debug("Deploy.Git.[" + g.opt.Branch + "].Commit.'" + message + "'")
+
+	// push to repo
+	_, stderr, err := com.ExecCmdDir(ctx.DstDir, "git", []string{
+		"push", "--force", "origin", g.opt.Branch}...)
+	if err != nil {
+		log15.Error("Deploy.Git.Error", "error", stderr)
+		if stderr != "" {
+			return errors.New(stderr)
+		}
+		return err
+	}
+	log15.Debug("Deploy.Git.[" + g.opt.Branch + "].Push")
 	return nil
 	/*
 		opt := g.opt
